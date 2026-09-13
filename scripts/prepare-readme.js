ca7086d8c819b072418b7bfa41e1a6286642afec
Dragate <spidfair@gmail.com>
2026-09-13 11:01:19 +0300

    fix: update README preparation script and package.json for npm publish

--- scripts/prepare-readme.js
+++ scripts/prepare-readme.js
@@ -1,15 +1,11 @@
 const fs = require('fs')
 const path = require('path')
 
-const packageJsonPath = path.join(__dirname, '../package.json')
 const readmePath = path.join(__dirname, '../README.md')
-
-const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'))
-const version = process.env.PACKAGE_VERSION || packageJson.version
 
 let readme = fs.readFileSync(readmePath, 'utf8')
 
-const cdnBase = `https://cdn.jsdelivr.net/npm/${packageJson.name}@${version}/docs/images/`
+const cdnBase = `https://6pcvnpdosyar2uqr.public.blob.vercel-storage.com/docs/images/`
 
 // Replace relative paths like ./docs/images/ or docs/images/ in attributes/markdown links with CDN base URL
 readme = readme.replace(/(["'(])(?:\.\/)?docs\/images\//g, `$1${cdnBase}`)
